b35d3346872d4183e76238aed745c68d0d3a6830
Heba Ali <hebaot.ali@gmail.com>
2023-02-10 22:09:23 +0300

    test fix

--- src/Store/index.ts
+++ src/Store/index.ts
@@ -1,4 +1,5 @@
 import { applyMiddleware, createStore } from "redux";
 import { reducer } from "../reducer/index";
 import thunk from "redux-thunk";
+
 export const store = createStore(reducer, applyMiddleware(thunk));
